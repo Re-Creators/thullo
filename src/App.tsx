01b55453c742474d27d1475c410cfcc17c5c91e2
Richardo <richardoehbet221@gmail.com>
2022-07-23 10:11:51 +0700

    chore : add headlessui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Menu } from "@headlessui/react";
 
 function App() {
   return (
     <div>
       <h1 className="text-red-500">Hello</h1>
+      <Menu>
+        <Menu.Button>More</Menu.Button>
+        <Menu.Items>
+          <Menu.Item disabled>
+            <span className="opacity-75">Invite a friend (coming soon!)</span>
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
     </div>
   );
 }
