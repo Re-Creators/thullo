be20934bfc06776d88941bcdbdd184d5bb0c4109
Re-Creators <richardoehbet221@gmail.com>
2022-09-14 10:26:53 +0700

    feat : display team member

--- src/components/board/Team.tsx
+++ src/components/board/Team.tsx
@@ -1,7 +1,9 @@
-import React from "react";
 import { BsFillFileEarmarkTextFill } from "react-icons/bs";
+import useBoardStore from "../../store/useBoardStore";
 
 export default function Team() {
+  const board = useBoardStore((state) => state.board);
+
   return (
     <div>
       <div className="text-sm flex items-center text-gray-400">
@@ -9,34 +11,30 @@ export default function Team() {
         <span className="ml-2">Team</span>
       </div>
       <div className="space-y-3">
-        <div className="mt-3 flex justify-between items-center">
-          <div className="flex items-center">
-            <div className="w-10 h-10 overflow-hidden rounded-lg">
-              <img
-                src="https://cdn.myanimelist.net/images/voiceactors/3/66042.jpg"
-                alt=""
-                className="w-full h-full object-cover object-center"
-              />
+        {board?.members.map((member) => (
+          <div
+            className="mt-3 flex justify-between items-center"
+            key={member.id}
+          >
+            <div className="flex items-center">
+              <div className="w-10 h-10 overflow-hidden rounded-lg">
+                <img
+                  src={member.profile.avatar_url}
+                  alt=""
+                  className="w-full h-full object-cover object-center"
+                />
+              </div>
+              <h2 className="text-sm ml-3">{member.profile.username}</h2>
             </div>
-            <h2 className="text-sm ml-3">Daniel Jansen</h2>
+            {board?.user_id === member.id ? (
+              <p className="text-xs text-gray-400">Admin</p>
+            ) : (
+              <button className="p-2 text-xs text-red-400 border border-red-500 rounded-lg hover:bg-red-500 hover:text-white">
+                Remove
+              </button>
+            )}
           </div>
-          <p className="text-xs text-gray-400">Admin</p>
-        </div>{" "}
-        <div className="mt-3 flex justify-between items-center">
-          <div className="flex items-center">
-            <div className="w-10 h-10 overflow-hidden rounded-lg">
-              <img
-                src="https://cdn.myanimelist.net/images/voiceactors/3/66042.jpg"
-                alt=""
-                className="w-full h-full object-cover object-center"
-              />
-            </div>
-            <h2 className="text-sm ml-3">Bianca Sosa</h2>
-          </div>
-          <button className="p-2 text-xs text-red-400 border border-red-500 rounded-lg hover:bg-red-500 hover:text-white">
-            Remove
-          </button>
-        </div>
+        ))}
       </div>
     </div>
   );
